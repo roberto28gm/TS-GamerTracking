0b61a3d8b58d8a7b2b7edabbfc8416d66c90c127
@roberto28gm <roberto28gm@gmail.com>
2023-01-04 19:57:04 +0100

    Added Boostrap, created Game Class, created basic Html table with some data

--- src/game.ts
+++ src/game.ts
@@ -1,1 +1,26 @@
 
+class Game {
+    protected id: number;
+    protected title: string;
+    protected score: number;
+    protected time: number;
+    protected finished: boolean;
+
+    constructor(id: number, title: string, score: number, time: number, finished: boolean) {
+        this.id = id;
+        this.title = title;
+        this.score = score;
+        this.time = time;
+        this.finished = finished;
+    }
+
+    AddGame(game: Game): void {
+        // TODO
+        // Add a new game to the list
+    }
+
+    DeleteGame(id: number): void {
+        // TODO
+        // Delete a game by Id
+    }
+}
